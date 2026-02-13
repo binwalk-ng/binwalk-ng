288b677e1923768e021bf91277847ff5cb2ba092
impervius <63152795+impervius@users.noreply.github.com>
2026-02-13 15:09:37 +0200

    Remove warning regarding comparing function pointers

--- src/binwalk.rs
+++ src/binwalk.rs
@@ -420,7 +420,7 @@ impl Binwalk {
          * want to process/validate the signatures. Each signature's parser function will report the correct
          * starting offset for the signature, so sort the file_map by the SignatureResult.offset value.
          */
-        file_map.sort();
+        file_map.sort_by_key(|e| e.offset);
         next_valid_offset = 0;
 
         /*
