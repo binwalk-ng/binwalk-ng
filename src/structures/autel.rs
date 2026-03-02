e2ef2a4bfda8f9535d5450ec4ecd6dc2da39f15f
impervius <63152795+impervius@users.noreply.github.com>
2026-03-02 08:15:46 +0200

    Migrate structures::autel to zerocopy

--- src/structures/autel.rs
+++ src/structures/autel.rs
@@ -1,5 +1,6 @@
 use crate::common::get_cstring;
-use crate::structures::common::{self, StructureError};
+use crate::structures::common::StructureError;
+use zerocopy::{FromBytes, Immutable, KnownLayout, LE, Unaligned};
 
 /// Struct to store Autel ECC header info
 #[derive(Debug, Default, Clone)]
@@ -8,38 +9,36 @@ pub struct AutelECCHeader {
     pub header_size: usize,
 }
 
+#[derive(FromBytes, KnownLayout, Unaligned, Immutable)]
+#[repr(C, packed)]
+struct AutelEccHeaderBytes {
+    magic: zerocopy::U64<LE>,
+    data_size: zerocopy::U32<LE>,
+    header_size: zerocopy::U32<LE>,
+    copyright: [u8; 16],
+}
+
 /// Parses an Autel header
 pub fn parse_autel_header(autel_data: &[u8]) -> Result<AutelECCHeader, StructureError> {
-    const EXPECTED_HEADER_SIZE: usize = 0x20;
-    const COPYRIGHT_SIZE: usize = 16;
+    const EXPECTED_HEADER_SIZE: u32 = 0x20;
     const EXPECTED_COPYRIGHT_STRING: &str = "Copyright Autel";
 
-    let autel_ecc_structure = vec![
-        ("magic", "u64"),
-        ("data_size", "u32"),
-        ("header_size", "u32"),
-        // Followed by 16-byte copyright string
-    ];
-
     // Parse the header
-    if let Ok(autel_header) = common::parse(autel_data, &autel_ecc_structure, "little") {
-        // Sanity check the reported header size
-        if autel_header["header_size"] == EXPECTED_HEADER_SIZE {
-            let copyright_start = common::size(&autel_ecc_structure);
-            let copyright_end = copyright_start + COPYRIGHT_SIZE;
+    let (autel_header, _) =
+        AutelEccHeaderBytes::ref_from_prefix(autel_data).map_err(|_| StructureError)?;
 
-            // Get the copyright string contained in the header
-            if let Some(copyright_bytes) = autel_data.get(copyright_start..copyright_end) {
-                let copyright_string = get_cstring(copyright_bytes);
+    // Sanity check the reported header size
+    if autel_header.header_size.get() == EXPECTED_HEADER_SIZE {
+        // Get the copyright string contained in the header
 
-                // Sanity check the copyright string value
-                if copyright_string == EXPECTED_COPYRIGHT_STRING {
-                    return Ok(AutelECCHeader {
-                        data_size: autel_header["data_size"],
-                        header_size: autel_header["header_size"],
-                    });
-                }
-            }
+        let copyright_string = get_cstring(&autel_header.copyright);
+
+        // Sanity check the copyright string value
+        if copyright_string == EXPECTED_COPYRIGHT_STRING {
+            return Ok(AutelECCHeader {
+                data_size: autel_header.data_size.get() as usize,
+                header_size: autel_header.header_size.get() as usize,
+            });
         }
     }
 
