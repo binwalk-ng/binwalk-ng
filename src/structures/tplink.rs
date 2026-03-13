f1221c1bfe3922eb85063026d5a029da4f151b13
impervius <63152795+impervius@users.noreply.github.com>
2026-03-13 20:22:02 +0200

    Migrate structures::tplink to zerocopy

--- src/structures/tplink.rs
+++ src/structures/tplink.rs
@@ -1,11 +1,40 @@
-use crate::structures::common::{self, StructureError};
+use crate::structures::common::StructureError;
+use zerocopy::{BE, FromBytes, Immutable, KnownLayout, LE, Unaligned};
 
 /// Stores info about a TP-Link firmware header
 #[derive(Debug, Default, Clone)]
 pub struct TPLinkFirmwareHeader {
     pub header_size: usize,
-    pub kernel_load_address: usize,
-    pub kernel_entry_point: usize,
+    pub kernel_load_address: u32,
+    pub kernel_entry_point: u32,
+}
+
+// https://github.com/jtreml/firmware-mod-kit/blob/master/src/tpl-tool/doc/Image_layout
+#[derive(FromBytes, KnownLayout, Unaligned, Immutable)]
+#[repr(C, packed)]
+struct TPLinkHeader {
+    product_id: zerocopy::U32<LE>,
+    product_version: zerocopy::U32<LE>,
+    reserved1: zerocopy::U32<LE>,
+    image_checksum_p1: zerocopy::U64<LE>,
+    image_checksum_p2: zerocopy::U64<LE>,
+    reserved2: zerocopy::U32<LE>,
+    kernel_checksum_p1: zerocopy::U64<LE>,
+    kernel_checksum_p2: zerocopy::U64<LE>,
+    reserved3: zerocopy::U32<LE>,
+    kernel_load_address: zerocopy::U32<LE>,
+    kernel_entry_point: zerocopy::U32<LE>,
+    image_length: zerocopy::U32<LE>,
+    kernel_offset: zerocopy::U32<LE>,
+    kernel_length: zerocopy::U32<LE>,
+    rootfs_offset: zerocopy::U32<LE>,
+    rootfs_length: zerocopy::U32<LE>,
+    bootloader_offset: zerocopy::U32<LE>,
+    bootloader_length: zerocopy::U32<LE>,
+    fw_version_major: zerocopy::U16<LE>,
+    fw_version_minor: zerocopy::U16<LE>,
+    fw_version_patch: zerocopy::U16<LE>,
+    reserved4: zerocopy::U32<LE>,
 }
 
 /// Pase a TP-Link firmware header
@@ -15,32 +44,6 @@ pub fn parse_tplink_header(tplink_data: &[u8]) -> Result<TPLinkFirmwareHeader, S
 
     // Total size of the firmware header
     const HEADER_SIZE: usize = 0x200;
-
-    // https://github.com/jtreml/firmware-mod-kit/blob/master/src/tpl-tool/doc/Image_layout
-    let tplink_structure = vec![
-        ("product_id", "u32"),
-        ("product_version", "u32"),
-        ("reserved1", "u32"),
-        ("image_checksum_p1", "u64"),
-        ("image_checksum_p2", "u64"),
-        ("reserved2", "u32"),
-        ("kernel_checksum_p1", "u64"),
-        ("kernel_checksum_p2", "u64"),
-        ("reserved3", "u32"),
-        ("kernel_load_address", "u32"),
-        ("kernel_entry_point", "u32"),
-        ("image_length", "u32"),
-        ("kernel_offset", "u32"),
-        ("kernel_length", "u32"),
-        ("rootfs_offset", "u32"),
-        ("rootfs_length", "u32"),
-        ("bootloader_offset", "u32"),
-        ("bootloader_length", "u32"),
-        ("fw_version_major", "u16"),
-        ("fw_version_minor", "u16"),
-        ("fw_version_patch", "u16"),
-        ("reserved4", "u32"),
-    ];
 
     let mut result = TPLinkFirmwareHeader {
         header_size: HEADER_SIZE,
@@ -52,18 +55,19 @@ pub fn parse_tplink_header(tplink_data: &[u8]) -> Result<TPLinkFirmwareHeader, S
         && let Some(structure_data) = tplink_data.get(STRUCTURE_OFFSET..)
     {
         // Parse the header
-        if let Ok(tplink_header) = common::parse(structure_data, &tplink_structure, "little") {
-            // Make sure the reserved fields are NULL
-            if tplink_header["reserved1"] == 0
-                && tplink_header["reserved2"] == 0
-                && tplink_header["reserved3"] == 0
-                && tplink_header["reserved4"] == 0
-            {
-                // Unfortunately, most header fields aren't reliably used; these seem to be, so report them
-                result.kernel_entry_point = tplink_header["kernel_entry_point"];
-                result.kernel_load_address = tplink_header["kernel_load_address"];
-                return Ok(result);
-            }
+        let (tplink_header, _) =
+            TPLinkHeader::ref_from_prefix(structure_data).map_err(|_| StructureError)?;
+
+        // Make sure the reserved fields are NULL
+        if tplink_header.reserved1.get() == 0
+            && tplink_header.reserved2.get() == 0
+            && tplink_header.reserved3.get() == 0
+            && tplink_header.reserved4.get() == 0
+        {
+            // Unfortunately, most header fields aren't reliably used; these seem to be, so report them
+            result.kernel_entry_point = tplink_header.kernel_entry_point.get();
+            result.kernel_load_address = tplink_header.kernel_load_address.get();
+            return Ok(result);
         }
     }
 
@@ -74,10 +78,24 @@ pub fn parse_tplink_header(tplink_data: &[u8]) -> Result<TPLinkFirmwareHeader, S
 #[derive(Debug, Default, Clone)]
 pub struct TPLinkRTOSFirmwareHeader {
     pub header_size: usize,
-    pub total_size: usize,
-    pub model_number: usize,
-    pub hardware_rev_major: usize,
-    pub hardware_rev_minor: usize,
+    pub total_size: u32,
+    pub model_number: u16,
+    pub hardware_rev_major: u8,
+    pub hardware_rev_minor: u8,
+}
+
+// https://github.com/jtreml/firmware-mod-kit/blob/master/src/tpl-tool/doc/Image_layout
+#[derive(FromBytes, KnownLayout, Unaligned, Immutable)]
+#[repr(C, packed)]
+struct TPLinkRTOSHeader {
+    magic1: zerocopy::U32<BE>,
+    unknown1: zerocopy::U64<BE>,
+    unknown2: zerocopy::U64<BE>,
+    magic2: zerocopy::U32<BE>,
+    data_size: zerocopy::U32<BE>,
+    model_number: zerocopy::U16<BE>,
+    hardware_revision_major: u8,
+    hardware_revision_minor: u8,
 }
 
 /// Parse a TP-Link RTOS firmware header
@@ -85,31 +103,20 @@ pub fn parse_tplink_rtos_header(
     tplink_data: &[u8],
 ) -> Result<TPLinkRTOSFirmwareHeader, StructureError> {
     const HEADER_SIZE: usize = 0x94;
-    const MAGIC2_VALUE: usize = 0x494D4730;
-    const TOTAL_SIZE_OFFSET: usize = 20;
+    const MAGIC2_VALUE: u32 = 0x494D4730;
+    const TOTAL_SIZE_OFFSET: u32 = 20;
 
-    let tplink_rtos_structure = vec![
-        ("magic1", "u32"),
-        ("unknown1", "u64"),
-        ("unknown2", "u64"),
-        ("magic2", "u32"),
-        ("data_size", "u32"),
-        ("model_number", "u16"),
-        ("hardware_revision_major", "u8"),
-        ("hardware_revision_minor", "u8"),
-    ];
+    let (header, _) = TPLinkRTOSHeader::ref_from_prefix(tplink_data).map_err(|_| StructureError)?;
 
-    if let Ok(header) = common::parse(tplink_data, &tplink_rtos_structure, "big")
-        && header["magic2"] == MAGIC2_VALUE
-    {
-        return Ok(TPLinkRTOSFirmwareHeader {
-            header_size: HEADER_SIZE,
-            total_size: header["data_size"] + TOTAL_SIZE_OFFSET,
-            model_number: header["model_number"],
-            hardware_rev_major: header["hardware_revision_major"],
-            hardware_rev_minor: header["hardware_revision_minor"],
-        });
+    if header.magic2.get() != MAGIC2_VALUE {
+        return Err(StructureError);
     }
 
-    Err(StructureError)
+    Ok(TPLinkRTOSFirmwareHeader {
+        header_size: HEADER_SIZE,
+        total_size: header.data_size.get() + TOTAL_SIZE_OFFSET,
+        model_number: header.model_number.get(),
+        hardware_rev_major: header.hardware_revision_major,
+        hardware_rev_minor: header.hardware_revision_minor,
+    })
 }
