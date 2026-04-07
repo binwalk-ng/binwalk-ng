36053c11d696ab094e475818b9fc30cac15e9172
impervius <63152795+impervius@users.noreply.github.com>
2026-04-07 21:39:07 +0300

    Migrate structures::uefi to zerocopy

--- src/structures/uefi.rs
+++ src/structures/uefi.rs
@@ -1,44 +1,47 @@
-use crate::structures::common::{self, StructureError};
+use crate::structures::common::StructureError;
+use zerocopy::{FromBytes, Immutable, KnownLayout, LE, Unaligned};
 
 /// Stores info about a UEFI volume header
 #[derive(Debug, Default, Clone)]
 pub struct UEFIVolumeHeader {
-    pub header_crc: usize,
+    pub header_crc: u16,
     pub header_size: usize,
     pub volume_size: usize,
+}
+
+#[derive(FromBytes, KnownLayout, Unaligned, Immutable)]
+#[repr(C, packed)]
+struct UEFIPiHeader {
+    volume_size: zerocopy::U64<LE>,
+    magic: zerocopy::U32<LE>,
+    attributes: zerocopy::U32<LE>,
+    header_size: zerocopy::U16<LE>,
+    header_crc: zerocopy::U16<LE>,
+    extended_header_offset: zerocopy::U16<LE>,
+    reserved: u8,
+    revision: u8,
 }
 
 /// Parse a UEFI volume header
 pub fn parse_uefi_volume_header(uefi_data: &[u8]) -> Result<UEFIVolumeHeader, StructureError> {
     // The revision field must be 1 or 2
-    let valid_revisions: Vec<usize> = vec![1, 2];
-
-    let uefi_pi_header_structure = vec![
-        ("volume_size", "u64"),
-        ("magic", "u32"),
-        ("attributes", "u32"),
-        ("header_size", "u16"),
-        ("header_crc", "u16"),
-        ("extended_header_offset", "u16"),
-        ("reserved", "u8"),
-        ("revision", "u8"),
-    ];
+    let valid_revisions: Vec<u8> = vec![1, 2];
 
     // Parse the volume header
-    if let Ok(uefi_volume_header) = common::parse(uefi_data, &uefi_pi_header_structure, "little") {
-        // Make sure the header size is sane (must be smaller than the total volume size)
-        if uefi_volume_header["header_size"] < uefi_volume_header["volume_size"] {
-            // The reserved field *must* be 0
-            if uefi_volume_header["reserved"] == 0 {
-                // The revision number must be 1 or 2
-                if valid_revisions.contains(&uefi_volume_header["revision"]) {
-                    return Ok(UEFIVolumeHeader {
-                        // TODO: Validate UEFI header CRC
-                        header_crc: uefi_volume_header["header_crc"],
-                        header_size: uefi_volume_header["header_size"],
-                        volume_size: uefi_volume_header["volume_size"],
-                    });
-                }
+    let (uefi_volume_header, _) =
+        UEFIPiHeader::ref_from_prefix(uefi_data).map_err(|_| StructureError)?;
+    // Make sure the header size is sane (must be smaller than the total volume size)
+    if (uefi_volume_header.header_size.get() as u64) < uefi_volume_header.volume_size.get() {
+        // The reserved field *must* be 0
+        if uefi_volume_header.reserved == 0 {
+            // The revision number must be 1 or 2
+            if valid_revisions.contains(&uefi_volume_header.revision) {
+                return Ok(UEFIVolumeHeader {
+                    // TODO: Validate UEFI header CRC
+                    header_crc: uefi_volume_header.header_crc.get(),
+                    header_size: uefi_volume_header.header_size.get() as usize,
+                    volume_size: uefi_volume_header.volume_size.get() as usize,
+                });
             }
         }
     }
@@ -53,25 +56,28 @@ pub struct UEFICapsuleHeader {
     pub header_size: usize,
 }
 
+#[derive(FromBytes, KnownLayout, Unaligned, Immutable)]
+#[repr(C, packed)]
+struct UEFICapsuleHeaderBytes {
+    guid_p1: zerocopy::U64<LE>,
+    guid_p2: zerocopy::U64<LE>,
+    header_size: zerocopy::U32<LE>,
+    flags: zerocopy::U32<LE>,
+    total_size: zerocopy::U32<LE>,
+}
+
 /// Parse  UEFI capsule header
 pub fn parse_uefi_capsule_header(uefi_data: &[u8]) -> Result<UEFICapsuleHeader, StructureError> {
-    let uefi_capsule_structure = vec![
-        ("guid_p1", "u64"),
-        ("guid_p2", "u64"),
-        ("header_size", "u32"),
-        ("flags", "u32"),
-        ("total_size", "u32"),
-    ];
-
     // Parse the capsule header
-    if let Ok(capsule_header) = common::parse(uefi_data, &uefi_capsule_structure, "little") {
-        // Sanity check on header and total size fields
-        if capsule_header["header_size"] < capsule_header["total_size"] {
-            return Ok(UEFICapsuleHeader {
-                total_size: capsule_header["total_size"],
-                header_size: capsule_header["header_size"],
-            });
-        }
+    let (capsule_header, _) =
+        UEFICapsuleHeaderBytes::ref_from_prefix(uefi_data).map_err(|_| StructureError)?;
+
+    // Sanity check on header and total size fields
+    if capsule_header.header_size.get() < capsule_header.total_size.get() {
+        return Ok(UEFICapsuleHeader {
+            total_size: capsule_header.total_size.get() as usize,
+            header_size: capsule_header.header_size.get() as usize,
+        });
     }
 
     Err(StructureError)
