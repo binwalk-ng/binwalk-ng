cb61fe3fab11e91cb940b24ea22b2f4d4a7ce62a
impervius <63152795+impervius@users.noreply.github.com>
2026-04-08 01:15:36 +0300

    Migrate structures::chk to zerocopy

--- src/structures/chk.rs
+++ src/structures/chk.rs
@@ -1,5 +1,6 @@
 use crate::common::get_cstring;
-use crate::structures::common::{self, StructureError};
+use crate::structures::common::StructureError;
+use zerocopy::{BE, FromBytes, Immutable, KnownLayout, Unaligned};
 
 /// Storage struct for CHK header info
 #[derive(Debug, Clone, Default)]
@@ -10,49 +11,51 @@ pub struct CHKHeader {
     pub board_id: String,
 }
 
+#[derive(FromBytes, KnownLayout, Unaligned, Immutable)]
+#[repr(C, packed)]
+struct CHKHeaderBytes {
+    magic: zerocopy::U32<BE>,
+    header_size: zerocopy::U32<BE>,
+    unknown: zerocopy::U64<BE>,
+    kernel_checksum: zerocopy::U32<BE>,
+    rootfs_checksum: zerocopy::U32<BE>,
+    rootfs_size: zerocopy::U32<BE>,
+    kernel_size: zerocopy::U32<BE>,
+    image_checksum: zerocopy::U32<BE>,
+    header_checksum: zerocopy::U32<BE>,
+    // Board ID string follows
+}
+
 /// Parse a CHK firmware header
 pub fn parse_chk_header(header_data: &[u8]) -> Result<CHKHeader, StructureError> {
     // Somewhat arbitrarily chosen
     const MAX_EXPECTED_HEADER_SIZE: usize = 100;
 
-    let chk_header_structure = vec![
-        ("magic", "u32"),
-        ("header_size", "u32"),
-        ("unknown", "u64"),
-        ("kernel_checksum", "u32"),
-        ("rootfs_checksum", "u32"),
-        ("rootfs_size", "u32"),
-        ("kernel_size", "u32"),
-        ("image_checksum", "u32"),
-        ("header_checksum", "u32"),
-        // Board ID string follows
-    ];
-
     // Size of the fixed-length portion of the header structure
-    let struct_size: usize = common::size(&chk_header_structure);
+    let struct_size: usize = std::mem::size_of::<CHKHeaderBytes>();
 
     // Parse the CHK header
-    if let Ok(chk_header) = common::parse(header_data, &chk_header_structure, "big") {
-        // Validate the reported header size
-        if chk_header["header_size"] > struct_size
-            && chk_header["header_size"] <= MAX_EXPECTED_HEADER_SIZE
-        {
-            // Read in the board ID string which immediately follows the fixed size structure and extends to the end of the header
-            let board_id_start: usize = struct_size;
-            let board_id_end: usize = chk_header["header_size"];
+    let (chk_header, _) =
+        CHKHeaderBytes::ref_from_prefix(header_data).map_err(|_| StructureError)?;
 
-            if let Some(board_id_raw_bytes) = header_data.get(board_id_start..board_id_end) {
-                let board_id_string = get_cstring(board_id_raw_bytes);
+    // Validate the reported header size
+    let header_size = chk_header.header_size.get() as usize;
+    if header_size > struct_size && header_size <= MAX_EXPECTED_HEADER_SIZE {
+        // Read in the board ID string which immediately follows the fixed size structure and extends to the end of the header
+        let board_id_start = struct_size;
+        let board_id_end = header_size;
 
-                // We expect that there must be a valid board ID string
-                if !board_id_string.is_empty() {
-                    return Ok(CHKHeader {
-                        board_id: board_id_string.clone(),
-                        header_size: chk_header["header_size"],
-                        kernel_size: chk_header["kernel_size"],
-                        rootfs_size: chk_header["rootfs_size"],
-                    });
-                }
+        if let Some(board_id_raw_bytes) = header_data.get(board_id_start..board_id_end) {
+            let board_id_string = get_cstring(board_id_raw_bytes);
+
+            // We expect that there must be a valid board ID string
+            if !board_id_string.is_empty() {
+                return Ok(CHKHeader {
+                    board_id: board_id_string,
+                    header_size: chk_header.header_size.get() as usize,
+                    kernel_size: chk_header.kernel_size.get() as usize,
+                    rootfs_size: chk_header.rootfs_size.get() as usize,
+                });
             }
         }
     }
