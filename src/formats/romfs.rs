ac412aaebaf2b3751b8a02726f17815986be25aa
impervius <63152795+impervius@users.noreply.github.com>
2026-08-02 00:51:06 +0300

    Remove two additional heap allocations

--- src/formats/romfs.rs
+++ src/formats/romfs.rs
@@ -89,11 +89,12 @@ pub fn parse_romfs_header(romfs_data: &[u8]) -> Result<RomFSHeader, StructureErr
             if let Some(crc_data) = romfs_data.get(0..crc_data_len)
                 && romfs_crc_valid(crc_data)
             {
+                // Volume name has a NULL terminator and is padded to a 16 byte boundary alignment
+                let header_size = header_size + romfs_align(volume_name.len() + 1);
                 return Ok(RomFSHeader {
                     image_size,
-                    volume_name: volume_name.clone(),
-                    // Volume name has a NULL terminator and is padded to a 16 byte boundary alignment
-                    header_size: header_size + romfs_align(volume_name.len() + 1),
+                    volume_name,
+                    header_size,
                 });
             }
         }
